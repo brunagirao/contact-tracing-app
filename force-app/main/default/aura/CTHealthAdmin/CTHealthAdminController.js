d1a5c450038d7d668303bd82b4ac08688b71a9a5
Bruna Girão <brunagirao.s@gmail.com>
2022-04-29 14:18:16 -0300

    CTHealthHeader - turn dinamic the header title

--- force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
+++ force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
@@ -1,5 +1,13 @@
 ({
-    myAction : function(component, event, helper) {
+    tabSelectHandler : function(component, event, helper) {
+        const selectedTabId = event.getParam("id");
 
+        if (selectedTabId === 'person') {
+            component.set("v.headerTitle", "Person View");
+        } else {
+            component.set("v.headerTitle", "Location View");
+        }
+        
+        component.set("v.scope", selectedTabId);
     }
 })
