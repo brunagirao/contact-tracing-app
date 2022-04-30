ce333a4cb3e147eeff6515ebcd49b12c22a54eb6
Bruna Girão <brunagirao.s@gmail.com>
2022-04-30 10:53:49 -0300

    CTHealthRecentChanges - add dinamic datatable

--- force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
+++ force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
@@ -1,5 +1,27 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit: function (component, event, helper) {
 
+        const scope = component.get("v.scope");
+
+        if (scope === "person") {
+            component.set("v.columns", [
+                { label: "Name", fieldName: "Name", type: "text" },
+                { label: "Phone", fieldName: "Mobile__c", type: "text" },
+                { label: "Token", fieldName: "Token__c", type: "text" },
+                { label: "Health Status", fieldName: "Health_Status__c", type: "text" },
+                { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" }
+            ]); 
+        } else {
+             component.set("v.columns", [
+                { label: "Name", fieldName: "Name", type: "text" },
+                { label: "Status", fieldName: "Status__c", type: "text" },
+                { label: "Red Score", fieldName: "Red_Score__c", type: "number" },
+                { label: "Pincode", fieldName: "Pincode__c", type: "text" },
+                { label: "Address", fieldName: "Address__c", type: "text" },
+                { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" }
+            ]);
+        }
+
+        helper.fetchRecentHealthChanges(component);
     }
 })
--- force-app/main/default/aura/CTRecentChanges/CTRecentChangesHelper.js
+++ force-app/main/default/aura/CTRecentChanges/CTRecentChangesHelper.js
@@ -1,5 +1,17 @@
 ({
-    helperMethod : function() {
+    fetchRecentHealthChanges: function (component, event, helper) {
+        
+       let action = component.get("v.scope") === "person" ? component.get("c.getRecentPersonHealthChanges") : component.get("c.getRecentLocationHealthChanges");
 
+       action.setCallback(this, function (response) {
+            const state = response.getState();
+
+            if (state === "SUCCESS") {
+                const data = response.getReturnValue();
+                component.set("v.data", data);
+            }
+        });
+        $A.enqueueAction(action);
     }
+
 })
