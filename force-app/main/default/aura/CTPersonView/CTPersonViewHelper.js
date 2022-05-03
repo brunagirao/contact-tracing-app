1dd5ce9bc821a9930eb1da86f19bb1dd605b9f3f
Bruna Girão <brunagirao.s@gmail.com>
2022-05-03 16:10:23 -0300

    CTPersonView - create updateHealthStatus n showToast functions

--- force-app/main/default/aura/CTPersonView/CTPersonViewHelper.js
+++ force-app/main/default/aura/CTPersonView/CTPersonViewHelper.js
@@ -1,5 +1,28 @@
 ({
-    helperMethod : function() {
+    updateHealthStatus : function (component, event, helper) {
+        let recordId = component.get("v.recordId");
 
+        let action = component.get("c.updateHealthStatus");
+        action.setParams({
+            personId: recordId
+        });
+
+        action.setCallback(this, function (response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                this.showToast("Success", "Person Health Status Updated!", "success");
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    showToast: function(title, message, type) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            title,
+            message,
+            type
+        });
+        toastEvent.fire();
     }
 })
