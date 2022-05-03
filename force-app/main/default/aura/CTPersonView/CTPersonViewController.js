4dc62c88da4550cd6791d41f1031de1872874d9f
Bruna Girão <brunagirao.s@gmail.com>
2022-05-03 16:09:52 -0300

    CTPersonView - create personSelectHandler n updateStatus functions

--- force-app/main/default/aura/CTPersonView/CTPersonViewController.js
+++ force-app/main/default/aura/CTPersonView/CTPersonViewController.js
@@ -1,5 +1,13 @@
 ({
-    myAction : function(component, event, helper) {
+    personSelectHandler: function(component, event, helper) {
+        let recordId = event.getParam("recordId");
+        let status = event.getParam("status");
 
+        component.set("v.recordId", recordId);
+        component.set("v.status", status);
+    },
+
+    updateStatus: function (component, event, helper) {
+        helper.updateHealthStatus(component);
     }
 })
