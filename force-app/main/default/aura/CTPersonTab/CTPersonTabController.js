f246c91f5c428a631df79816c8edadd88c5d1b0e
Bruna Girão <brunagirao.s@gmail.com>
2022-05-10 08:39:36 -0300

    CTPersonTab - create logic to search person details traces

--- force-app/main/default/aura/CTPersonTab/CTPersonTabController.js
+++ force-app/main/default/aura/CTPersonTab/CTPersonTabController.js
@@ -1,5 +1,15 @@
 ({
-    myAction : function(component, event, helper) {
+    userSearchHandler : function(component, event, helper) {
+        helper.fetchUserInformation(component);
+    },
 
+    doInit : function(component, event, helper) {
+        const columns = [
+            { label: "Token", fieldName: "token", type: "text" },
+            { label: "Contact Status", fieldName: "status", type: "text" },
+            { label: "Contact Date", fieldName: "contactDate", type: "date" }
+        ];
+
+        component.set("v.columns", columns);
     }
 })
