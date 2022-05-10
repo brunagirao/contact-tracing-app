d1ed1131da92d1233e5b4698c130ed687ab63bdc
Bruna Girão <brunagirao.s@gmail.com>
2022-05-10 17:12:52 -0300

    CTLocationTab - create logic show visitor in the location

--- force-app/main/default/aura/CTLocationTab/CTLocationTabController.js
+++ force-app/main/default/aura/CTLocationTab/CTLocationTabController.js
@@ -1,5 +1,14 @@
 ({
-    myAction : function(component, event, helper) {
+    locationSearchHandler : function(component, event, helper) {
+        helper.fetchLocationInformation(component);
+    },
 
+    doInit: function (component, event, helper) {
+        const columns = [
+            { label: "Token", fieldName: "token", type: "text" },
+            { label: "Contact Status", fieldName: "status", type: "text" },
+            { label: "Visit Date", fieldName: "visitDate", type: "date" }
+        ];
+        component.set("v.columns", columns);
     }
 })
--- force-app/main/default/aura/CTLocationTab/CTLocationTabHelper.js
+++ force-app/main/default/aura/CTLocationTab/CTLocationTabHelper.js
@@ -1,5 +1,42 @@
 ({
-    helperMethod : function() {
+    fetchLocationInformation: function (component, event, helper) {
+        //Method definition goes here
+        const recordId = component.get("v.recordId");
+        const action = component.get("c.getLocationDetails");
+        action.setParams({
+            locationId: recordId
+        });
+        action.setCallback(this, function (response) {
+            const state = response.getState();
+            if (state === "SUCCESS") {
+                const resp = response.getReturnValue();
+                // check if location found
+                if (!resp || !resp.name) {
+                    // location not found
+                    component.set("v.locationFound", false);
+                    this.showToast("ERROR", "Please enter valid location id", "error");
+                } else {
+                    // location found
+                    component.set("v.locationFound", true);
+                    component.set("v.locationInfo", resp);
+                }
+            } else {
+                component.set("v.locationFound", false);
+                this.showToast("ERROR", "Please enter valid location id", "error");
+            }
+            console.log(omponent.get("!v.locationInfo"));
+        });
+        $A.enqueueAction(action);
+    },
 
+    showToast: function (title, message, type) {
+        //Method definition goes here
+        const toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            title,
+            message,
+            type
+        });
+        toastEvent.fire();
     }
-})
+});
